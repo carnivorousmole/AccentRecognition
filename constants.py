ccd7b2995985cf3e2ea055f14e9b6f9554e9013b
Dylan Walsh <dylanwitsend@gmail.com>
2023-03-19 16:28:59 +0000

    added split audio path in comment

--- constants.py
+++ constants.py
@@ -7,6 +7,7 @@ AUDIOS_INFO_FILE_NAME = './mfccs.csv'
 FILTERED_AUDIOS_INFO_FILE_NAME = './filtered_mfccs.csv'
 SAMPLE_FILE_NAME = '{}.mp3'
 AUDIO_INPUT_PATH = "/Users/dylanwalsh/Code/input/audio_files/audios"
+# AUDIO_INPUT_PATH = "/Users/dylanwalsh/Code/input/audio_files/audios_word_split_reorganized/please_call_Stella_"
 SAVED_FEATURES_PATH = "./features/isolated_features/"
 
 LANGUAGES = {
